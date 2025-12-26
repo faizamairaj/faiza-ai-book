4d79129e17992f78a5ae25b16e695e4686f0f8b5
Faizamairaj <160303168+faizamairaj@users.noreply.github.com>
2025-12-26 12:52:57 +0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://faizamairaj.github.io",
+  url: "https://faizamairaj.github.io/faiza-ai-book",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/faiza-ai-book/",
